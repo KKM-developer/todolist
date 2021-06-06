175b4806b4b389f6a507de783d80761068bdf9a4
KKM-dev <cirill@mail.ru>
2021-06-06 17:53:34 +0300

    hw lesson 2

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
+    'corsheaders',
 
     'users',
 ]
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'todolist.urls'
@@ -129,3 +132,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.NewUser'
+
+CORS_ALLOWED_ORIGINS = [
+   "http://localhost:3000",
+]
